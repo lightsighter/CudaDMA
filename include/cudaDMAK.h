1870439cc3ed5249862138e989dec3b75419b027
Mike Bauer <mebauer@cs.stanford.edu>
2012-08-31 16:47:02 -0700

    Adding code for the big elements case for the new Kepler version

--- include/cudaDMAK.h
+++ include/cudaDMAK.h
@@ -121,12 +121,28 @@ protected:
 #define ELMT_PER_STEP_SPLIT ((DMA_THREADS/THREADS_PER_ELMT) * MAX_LDS_PER_THREAD)
 #define ROW_ITERS_SPLIT	 (MAX_LDS_PER_THREAD)
 #define HAS_PARTIAL_ELMTS_SPLIT ((NUM_ELMTS % ELMT_PER_STEP_SPLIT) != 0)
-#define HAS_PARTIAL_BYTES_SPLIT ((LDS_PER_ELMT % THREADS_PER_ELMT) != 0)
+//#define HAS_PARTIAL_BYTES_SPLIT ((LDS_PER_ELMT % THREADS_PER_ELMT) != 0)
+#define HAS_PARTIAL_BYTES_SPLIT ((BYTES_PER_ELMT % (THREADS_PER_ELMT*ALIGNMENT)) != 0)
 #define COL_ITERS_SPLIT  ((LDS_PER_ELMT == THREADS_PER_ELMT) ? 1 : 0)
-#define STEP_ITERS_SPLIT ((NUM_ELMTS >= ELMT_PER_STEP_SPLIT) ? NUM_ELMTS/ELMT_PER_STEP_SPLIT : \
-                                                               NUM_ELMTS/(DMA_THREADS/THREADS_PER_ELMT))
+#define STEP_ITERS_SPLIT (NUM_ELMTS/ELMT_PER_STEP_SPLIT)
 
 #define NUM_WARPS (DMA_THREADS/WARP_SIZE)
+
+// Next we'll handle the case where all the warps performing as many loads as
+// possible in a step can't handle an entire element.
+#define BIG_ELMTS ((DMA_THREADS*MAX_LDS_PER_THREAD) < LDS_PER_ELMT)
+// This is the number of steps where ALL threads can perform the maximum number of loads
+#define MAX_ITERS_BIG (LDS_PER_ELMT/(DMA_THREADS*MAX_LDS_PER_THREAD))
+// This is actually whether there are leftovers from the max loading phase
+#define HAS_PARTIAL_ELMTS_BIG (((LDS_PER_ELMT % (DMA_THREADS*MAX_LDS_PER_THREAD)) / DMA_THREADS) > 0)
+// This is the number of loads to be performed for remaining bytes after max loads
+#define PART_ITERS_BIG ((LDS_PER_ELMT % (DMA_THREADS*MAX_LDS_PER_THREAD)) / DMA_THREADS)
+#define REMAINING_BYTES_BIG (BYTES_PER_ELMT - (MAX_ITERS_BIG*DMA_THREADS*MAX_LDS_PER_THREAD*ALIGNMENT + \
+                                                  PART_ITERS_BIG*DMA_THREADS*ALIGNMENT))
+// This is actually whether there are leftovers from the partial loading phase
+#define HAS_PARTIAL_BYTES_BIG (REMAINING_BYTES_BIG > 0)
+#define STEP_ITERS_BIG (NUM_ELMTS)
+
 // Now handle the case where we don't have to split a warp across multiple elements.
 // Now see if we can have a single warp handle an entire element in a single pass.
 // We only do this if every warp will be busy, otherwise we'll allocate warps to elements
@@ -141,29 +157,35 @@ protected:
 // to maximize MLP, if we exceed the maximum, then split the group of warps across
 // multiple elements.  Once we've allocated warps to elements, see how many elements we
 // can handle based on the number of outstanding loads each thread can have.
+// Include the BIG_ELMTS case to avoid divide by zero errors in template instantiation.
 #define MAX_WARPS_PER_ELMT ((LDS_PER_ELMT+WARP_SIZE-1)/WARP_SIZE)
 #define WARPS_PER_ELMT (SINGLE_WARP ? 1 : \
+                        BIG_ELMTS ? NUM_WARPS : \
                         (NUM_WARPS <= NUM_ELMTS) ? MINIMUM_COVER : \
                         ((MAX_WARPS_PER_ELMT >= NUM_WARPS) ? NUM_WARPS : MAX_WARPS_PER_ELMT))
 // Figure out how many loads need to be done per thread per element (round up)
 #define LDS_PER_ELMT_PER_THREAD ((LDS_PER_ELMT+(WARPS_PER_ELMT*WARP_SIZE)-1)/(WARPS_PER_ELMT*WARP_SIZE))
 // This assumes that the number of warps allocated to the element were enough to
-// cover the size of the element.
-#define ELMT_PER_STEP_PER_THREAD (MAX_LDS_PER_THREAD/LDS_PER_ELMT_PER_THREAD)
+// cover the size of the element. Note we mask out the result if we're in a BIG_ELMTS
+// case because it can lead to divide by zero errors in the template instantiation.
+#define ELMT_PER_STEP_PER_THREAD (BIG_ELMTS ? 1 : MAX_LDS_PER_THREAD/LDS_PER_ELMT_PER_THREAD)
 // Now we can figure out how many elements we can handle per step by multiplying
 // the total number of elements to be handled by each thread in a step by
 // the total number of groups of warps (also total groups of threads)
 #define ELMT_PER_STEP_FULL (ELMT_PER_STEP_PER_THREAD * (NUM_WARPS/WARPS_PER_ELMT))
 #define ROW_ITERS_FULL (ELMT_PER_STEP_PER_THREAD)
 #define HAS_PARTIAL_ELMTS_FULL ((NUM_ELMTS % ELMT_PER_STEP_FULL) != 0)
-#define HAS_PARTIAL_BYTES_FULL ((LDS_PER_ELMT % (WARPS_PER_ELMT*WARP_SIZE)) != 0)
+//#define HAS_PARTIAL_BYTES_FULL ((LDS_PER_ELMT % (WARPS_PER_ELMT*WARP_SIZE)) != 0)
+#define HAS_PARTIAL_BYTES_FULL ((BYTES_PER_ELMT % (WARPS_PER_ELMT*WARP_SIZE*ALIGNMENT)) != 0)
 #define COL_ITERS_FULL (LDS_PER_ELMT/(WARPS_PER_ELMT*WARP_SIZE))
 //#define STEP_ITERS_FULL ((NUM_ELMTS >= ELMT_PER_STEP_FULL) ? NUM_ELMTS/ELMT_PER_STEP_FULL : \
 //                                                             NUM_ELMTS/(NUM_WARPS/WARPS_PER_ELMT))
 #define STEP_ITERS_FULL (NUM_ELMTS/ELMT_PER_STEP_FULL)
 
-#define HAS_PARTIAL_BYTES (SPLIT_WARP ? HAS_PARTIAL_BYTES_SPLIT : HAS_PARTIAL_BYTES_FULL)
-#define HAS_PARTIAL_ELMTS  (SPLIT_WARP ? HAS_PARTIAL_ELMTS_SPLIT : HAS_PARTIAL_ELMTS_FULL)
+#define HAS_PARTIAL_BYTES (SPLIT_WARP ? HAS_PARTIAL_BYTES_SPLIT : \
+                           BIG_ELMTS ? HAS_PARTIAL_BYTES_BIG : HAS_PARTIAL_BYTES_FULL)
+#define HAS_PARTIAL_ELMTS (SPLIT_WARP ? HAS_PARTIAL_ELMTS_SPLIT : \
+                           BIG_ELMTS ? HAS_PARTIAL_BYTES_BIG : HAS_PARTIAL_ELMTS_FULL)
 
 // Finally, let's compute all the initial values based on the things above.
 // First we'll do the split versions
@@ -193,7 +215,21 @@ protected:
 #define INIT_PARTIAL_ELMTS_SPLIT (FULL_REMAINING_SPLIT + \
                                   ((LAST_REMAINING_SPLIT==0) ? 0 : \
                                    ((ELMT_ID_SPLIT >= LAST_REMAINING_SPLIT) ? 0 : 1)))
-// Now we do the non-split versions
+
+// Now for the big versions
+#define INIT_SRC_OFFSET_BIG (CUDADMA_DMA_TID * ALIGNMENT)
+#define INIT_DST_OFFSET_BIG (CUDADMA_DMA_TID * ALIGNMENT)
+#define INIT_SRC_STEP_STRIDE_BIG(_src_stride) (_src_stride)
+#define INIT_DST_STEP_STRIDE_BIG(_dst_stride) (_dst_stride)
+#define INIT_SRC_ELMT_STRIDE_BIG(_src_stride) (_src_stride)
+#define INIT_DST_ELMT_STRIDE_BIG(_dst_stride) (_dst_stride)
+#define INIT_INTRA_ELMT_STRIDE_BIG (DMA_THREADS * ALIGNMENT)
+#define INIT_PARTIAL_ELMTS_BIG (0) // No partial elements in the big elements case
+#define INIT_PARTIAL_BYTES_BIG ((CUDADMA_DMA_TID > (REMAINING_BYTES_BIG/ALIGNMENT)) ? 0 : \
+                                (CUDADMA_DMA_TID == (REMAINING_BYTES_BIG/ALIGNMENT)) ? \
+                                                  (BYTES_PER_ELMT - (FULL_LDS_PER_ELMT * ALIGNMENT)) : ALIGNMENT)
+
+// Now we do the full versions 
 #define ELMT_ID_FULL (CUDADMA_DMA_TID/(WARPS_PER_ELMT*WARP_SIZE))
 #define FULL_GROUP_TID (threadIdx.x - (dma_threadIdx_start + (ELMT_ID_FULL * WARPS_PER_ELMT * WARP_SIZE)))
 #define INIT_SRC_OFFSET_FULL(_src_stride) (ELMT_ID_FULL * _src_stride + FULL_GROUP_TID * ALIGNMENT)
@@ -224,20 +260,26 @@ protected:
 #define INIT_ACTIVE_WARP (WARP_ID < NUM_ACTIVE_WARPS)
 #define ALL_WARPS_ACTIVE (NUM_WARPS == NUM_ACTIVE_WARPS) 
 
-#define INIT_SRC_OFFSET(_src_stride) (SPLIT_WARP ? INIT_SRC_OFFSET_SPLIT(_src_stride) : INIT_SRC_OFFSET_FULL(_src_stride))
-#define INIT_DST_OFFSET(_dst_stride) (SPLIT_WARP ? INIT_DST_OFFSET_SPLIT(_dst_stride) : INIT_DST_OFFSET_FULL(_dst_stride))
+#define INIT_SRC_OFFSET(_src_stride) (SPLIT_WARP ? INIT_SRC_OFFSET_SPLIT(_src_stride) : \
+                                      BIG_ELMTS  ? INIT_SRC_OFFSET_BIG : INIT_SRC_OFFSET_FULL(_src_stride))
+#define INIT_DST_OFFSET(_dst_stride) (SPLIT_WARP ? INIT_DST_OFFSET_SPLIT(_dst_stride) : \
+                                      BIG_ELMTS  ? INIT_DST_OFFSET_BIG : INIT_DST_OFFSET_FULL(_dst_stride))
 #define INIT_SRC_STEP_STRIDE(_src_stride) (SPLIT_WARP ? INIT_SRC_STEP_STRIDE_SPLIT(_src_stride) : \
-                                                        INIT_SRC_STEP_STRIDE_FULL(_src_stride))
+                                           BIG_ELMTS  ? INIT_SRC_STEP_STRIDE_BIG(_src_stride) : INIT_SRC_STEP_STRIDE_FULL(_src_stride))
 #define INIT_DST_STEP_STRIDE(_dst_stride) (SPLIT_WARP ? INIT_DST_STEP_STRIDE_SPLIT(_dst_stride) : \
-                                                        INIT_DST_STEP_STRIDE_FULL(_dst_stride))
+                                           BIG_ELMTS  ? INIT_DST_STEP_STRIDE_BIG(_dst_stride) : INIT_DST_STEP_STRIDE_FULL(_dst_stride))
 #define INIT_SRC_ELMT_STRIDE(_src_stride) (SPLIT_WARP ? INIT_SRC_ELMT_STRIDE_SPLIT(_src_stride) : \
-                                                        INIT_SRC_ELMT_STRIDE_FULL(_src_stride))
+                                           BIG_ELMTS  ? INIT_SRC_ELMT_STRIDE_BIG(_src_stride) : INIT_SRC_ELMT_STRIDE_FULL(_src_stride))
 #define INIT_DST_ELMT_STRIDE(_dst_stride) (SPLIT_WARP ? INIT_DST_ELMT_STRIDE_SPLIT(_dst_stride) : \
-                                                        INIT_DST_ELMT_STRIDE_FULL(_dst_stride))
-#define INIT_INTRA_ELMT_STRIDE (SPLIT_WARP ? INIT_INTRA_ELMT_STRIDE_SPLIT : INIT_INTRA_ELMT_STRIDE_FULL)
-#define INIT_PARTIAL_BYTES (SPLIT_WARP ? INIT_PARTIAL_BYTES_SPLIT : INIT_PARTIAL_BYTES_FULL)
-#define INIT_PARTIAL_ELMTS (SPLIT_WARP ? INIT_PARTIAL_ELMTS_SPLIT : INIT_PARTIAL_ELMTS_FULL)
-#define INIT_PARTIAL_OFFSET (SPLIT_WARP ? 0 : ((FULL_LDS_PER_ELMT - (FULL_LDS_PER_ELMT % (WARPS_PER_ELMT * WARP_SIZE))) * ALIGNMENT))
+                                           BIG_ELMTS  ? INIT_DST_ELMT_STRIDE_BIG(_dst_stride) : INIT_DST_ELMT_STRIDE_FULL(_dst_stride))
+#define INIT_INTRA_ELMT_STRIDE (SPLIT_WARP ? INIT_INTRA_ELMT_STRIDE_SPLIT : \
+                                BIG_ELMTS  ? INIT_INTRA_ELMT_STRIDE_BIG : INIT_INTRA_ELMT_STRIDE_FULL)
+#define INIT_PARTIAL_BYTES (SPLIT_WARP ? INIT_PARTIAL_BYTES_SPLIT : \
+                            BIG_ELMTS  ? INIT_PARTIAL_BYTES_BIG : INIT_PARTIAL_BYTES_FULL)
+#define INIT_PARTIAL_ELMTS (SPLIT_WARP ? INIT_PARTIAL_ELMTS_SPLIT : \
+                            BIG_ELMTS  ? INIT_PARTIAL_ELMTS_BIG : INIT_PARTIAL_ELMTS_FULL)
+#define INIT_PARTIAL_OFFSET (SPLIT_WARP ? 0 : BIG_ELMTS ? 0 : \
+                            ((FULL_LDS_PER_ELMT - (FULL_LDS_PER_ELMT % (WARPS_PER_ELMT * WARP_SIZE))) * ALIGNMENT))
 
 #include <cstdio>
 template<int ALIGNMENT, int BYTES_PER_THREAD, int BYTES_PER_ELMT, int DMA_THREADS, int NUM_ELMTS>
@@ -248,6 +290,7 @@ __host__ void print_strided_variables(void)
   PRINT_VAR(LDS_PER_ELMT);
   PRINT_VAR(FULL_LDS_PER_ELMT);
   PRINT_VAR(SPLIT_WARP);
+  PRINT_VAR(BIG_ELMTS);
   printf("---------- Split math ---------\n");
   PRINT_VAR(THREADS_PER_ELMT);
   PRINT_VAR(ELMT_PER_STEP_SPLIT);
@@ -256,6 +299,13 @@ __host__ void print_strided_variables(void)
   PRINT_VAR(HAS_PARTIAL_BYTES_SPLIT);
   PRINT_VAR(COL_ITERS_SPLIT);
   PRINT_VAR(STEP_ITERS_SPLIT); 
+  printf("---------- Big math -----------\n");
+  PRINT_VAR(HAS_PARTIAL_ELMTS_BIG);
+  PRINT_VAR(HAS_PARTIAL_BYTES_BIG);
+  PRINT_VAR(MAX_ITERS_BIG);
+  PRINT_VAR(PART_ITERS_BIG);
+  PRINT_VAR(STEP_ITERS_BIG);
+  PRINT_VAR(REMAINING_BYTES_BIG);
   printf("---------- Full math ----------\n");
   PRINT_VAR(NUM_WARPS);
   PRINT_VAR(SINGLE_WARP);
@@ -340,23 +390,29 @@ public:
     {
       case 4:
       	{
-          execute_internal<float,SPLIT_WARP,STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
-	  				    STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
-					    HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
+          execute_internal<float,SPLIT_WARP,BIG_ELMTS,
+                                  STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
+                                  STEP_ITERS_BIG,MAX_ITERS_BIG,PART_ITERS_BIG,
+	  			  STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
+				  HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
 	  break;
 	}
     case 8:
       	{
-	  execute_internal<float2,SPLIT_WARP,STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
-	  			  	     STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
-					     HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
+	  execute_internal<float2,SPLIT_WARP,BIG_ELMTS,
+                                  STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
+                                  STEP_ITERS_BIG,MAX_ITERS_BIG,PART_ITERS_BIG,
+	  			  STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
+				  HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
 	  break;
 	}
     case 16:
       	{
-	  execute_internal<float4,SPLIT_WARP,STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
-	  			  	     STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
-					     HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
+	  execute_internal<float4,SPLIT_WARP,BIG_ELMTS,
+                                  STEP_ITERS_SPLIT,ROW_ITERS_SPLIT,COL_ITERS_SPLIT,
+                                  STEP_ITERS_BIG,MAX_ITERS_BIG,PART_ITERS_BIG,
+	  			  STEP_ITERS_FULL,ROW_ITERS_FULL,COL_ITERS_FULL,
+				  HAS_PARTIAL_BYTES,HAS_PARTIAL_ELMTS,ALL_WARPS_ACTIVE>(src_ptr,dst_ptr);
 	  break;
 	}
 #ifdef CUDADMA_DEBUG_ON
@@ -368,8 +424,9 @@ public:
     }
   }
 
-  template<typename BULK_TYPE, bool DMA_IS_SPLIT,
+  template<typename BULK_TYPE, bool DMA_IS_SPLIT, bool DMA_IS_BIG,
            int DMA_STEP_ITERS_SPLIT, int DMA_ROW_ITERS_SPLIT, int DMA_COL_ITERS_SPLIT,
+           int DMA_STEP_ITERS_BIG,   int DMA_MAX_ITERS_BIG,   int DMA_PART_ITERS_BIG,
            int DMA_STEP_ITERS_FULL,  int DMA_ROW_ITERS_FULL,  int DMA_COL_ITERS_FULL,
 	   bool DMA_PARTIAL_BYTES, bool DMA_PARTIAL_ROWS, bool DMA_ALL_WARPS_ACTIVE>
   __device__ __forceinline__ void execute_internal(const void *RESTRICT src_ptr, void *RESTRICT dst_ptr) const
@@ -401,7 +458,20 @@ public:
         }
       }
     }
-    else // Not split
+    else if (DMA_IS_BIG)
+    {
+      // No optimized case here, we have at least one full column iteration to perform
+      if (DO_SYNC_TOP)
+        CUDADMA_BASE::wait_for_dma_start();
+      for (int i = 0; i < DMA_STEP_ITERS_BIG; i++)
+      {
+        do_copy_elmt<BULK_TYPE,DMA_MAX_ITERS_BIG,DMA_PART_ITERS_BIG,DMA_PARTIAL_ROWS,DMA_PARTIAL_BYTES>
+            (src_off_ptr, dst_off_ptr, dma_intra_elmt_stride, dma_partial_bytes);
+        src_off_ptr += dma_src_elmt_stride;
+        dst_off_ptr += dma_dst_elmt_stride;
+      }
+    }
+    else // Not split and not big
     {
       if (DMA_ALL_WARPS_ACTIVE) // Check to see if all the warps are active
       {
@@ -550,6 +620,80 @@ public:
 	dst_row_ptr += dst_elmt_stride;
       }
     }
+  }
+
+  template<typename BULK_TYPE, int DMA_MAX_ITERS, int DMA_PART_ITERS, bool DMA_PARTIAL_ITERS, bool DMA_PARTIAL_BYTES>
+  __device__ __forceinline__ void do_copy_elmt(const char *RESTRICT src_ptr, char *RESTRICT dst_ptr,
+                                               const int intra_elmt_stride, const int partial_bytes) const
+  {
+#define MAX_LOADS (BYTES_PER_THREAD/sizeof(BULK_TYPE))
+    BULK_TYPE temp[GUARD_ZERO(MAX_LOADS)];
+#define PERFORM_MEMORY_OPS(_num_loads)                \
+    for (int idx = 0; idx < _num_loads; idx++)        \
+    {                                                 \
+      perform_load<BULK_TYPE>(src_ptr, &(temp[idx])); \
+      src_ptr += intra_elmt_stride;                   \
+    }                                                 \
+    for (int idx = 0; idx < _num_loads; idx++)        \
+    {                                                 \
+      perform_store<BULK_TYPE>(&(temp[idx]), dst_ptr);\
+      dst_ptr += intra_elmt_stride;                   \
+    }
+    // First perform as many max iters as we can
+    for (int i = 0; i < DMA_MAX_ITERS; i++)
+    {
+      PERFORM_MEMORY_OPS(MAX_LOADS);
+    }
+#undef MAX_LOADS
+    if (DMA_PARTIAL_ITERS)
+    {
+      for (int i = 0; i < DMA_PARTIAL_ITERS; i++)
+      {
+        PERFORM_MEMORY_OPS(DMA_PART_ITERS);
+      }
+    }
+    if (DMA_PARTIAL_BYTES)
+    {
+      switch (partial_bytes)
+      {
+        case 0:
+          break;
+        case 4:
+          {
+            float tmp;
+            perform_load<float>(src_ptr,&tmp);
+            perform_store<float>(&tmp,dst_ptr);
+            break;
+          }
+        case 8:
+          {
+            float2 tmp;
+            perform_load<float2>(src_ptr,&tmp);
+            perform_store<float2>(&tmp,dst_ptr);
+            break; 
+          }
+        case 12:
+          {
+            float3 tmp;
+            perform_load<float3>(src_ptr,&tmp);
+            perform_store<float3>(&tmp,dst_ptr);
+            break;
+          }
+        case 16:
+          {
+            float4 tmp;
+            perform_load<float4>(src_ptr,&tmp);
+            perform_store<float4>(&tmp,dst_ptr);
+            break;
+          }
+#ifdef CUDADMA_DEBUG_ON
+        default:
+          printf("Invalid partial bytes size (%d) for strided across.\n" partial_bytes);
+          assert(false);
+#endif
+      }
+    }
+#undef PERFORM_MEMORY_OPS
   }
 
   template<bool DO_SYNC, int DMA_ROW_ITERS>
